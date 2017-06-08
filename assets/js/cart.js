fda6a322b7ad08b73c40ab39ba4a37023a3e76b6
Ferran Basora <fcsonline@gmail.com>
2017-06-08 14:45:27 +0000

    Coupons

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -6,39 +6,68 @@
         }
 
         function updateCart(data) {
-          var formated = (data.amount / 100).toFixed(2) + '€';
+          var coupon = data.coupon;
 
-          $('.cart .items').val(data.items);
+          $('.cart select[name=quantity]').val(data.items);
           $('.cart #payment').data('amount', data.amount);
           $('.cart #payment').attr('data-amount', data.amount);
           $('.cart #subtotal').text(formatPrice(data.subtotal));
           $('.cart #total').text(formatPrice(data.amount));
           $('.cart #taxes').text(formatPrice(data.taxes));
+          $('.cart #discount').text(formatPrice(data.discount));
           $('.cart #price').text(formatPrice(data.price));
+          $('.cart #coupon-reason').text(data.coupon_reason);
+
+          if (coupon) {
+            $('.cart #coupon-label').text(data.coupon).show();
+            $('.cart #coupon').hide();
+          }
+        }
+
+        function disablePayButton(value) {
+          $('#checkout button').attr('disabled', value);
         }
 
         $.get('/cart', { locale: $('html').attr('lang') }).done(function (data) {
           updateCart(data);
+
+          $('.cart #loading').hide();
+          $('.cart #detail').show();
         });
 
-        $('.cart .add').click(function(event) {
-          $.post('/cart/add').done(function (data) {
+        $('.cart select[name=quantity]').change(function(event) {
+          disablePayButton(true);
+
+          $.post('/cart/quantity', {quantity: $(this).val()}).done(function (data) {
             updateCart(data);
+            disablePayButton(false);
           });
 
           event.preventDefault();
         });
 
-        $('.cart .subtract').click(function(event) {
-          $.post('/cart/subtract').done(function (data) {
+        $('.cart #checkout').submit(function( event ) {
+          $('.cart #detail').hide();
+          $('.cart #loading').show();
+        });
+
+        $('.cart #coupon').submit(function(event) {
+          var $coupon_form = $('.cart #coupon');
+          var $coupon_input = $('.cart #coupon input[name=coupon]');
+          var $coupon_reason = $('.cart #coupon-reason');
+
+          $.post('/cart/coupon', {coupon: $coupon_input.val()}).done(function (data) {
+            if (data.coupon) {
+              $coupon_form.hide();
+              $coupon_reason.text('');
+            }
+
             updateCart(data);
           });
 
           event.preventDefault();
-        });
 
-        $('.cart form').submit(function( event ) {
-          $('.cart .loading').show();
+          return false;
         });
     });
 
